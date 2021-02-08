d3ac646b0b89aab01a99bce0fc3b2b0e6450a9ef
Alpha <alphatero@icloud.com>
2021-02-08 11:09:30 +0800

    upload issue

--- vue-pj/src/views/backEnd/Dashboard.vue
+++ vue-pj/src/views/backEnd/Dashboard.vue
@@ -2,6 +2,7 @@
     <div class="container-fluid">
       <div class="row">
         <Sidebar/>
+        <Alert/>
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4 mt-5">
           <router-view></router-view>
         </main>
@@ -11,10 +12,12 @@
 
 <script>
 import Sidebar from '@/components/Sidebar.vue'
+import Alert from '@/components/alertMessage.vue'
 export default {
   name: 'Dashboard',
   components: {
-    Sidebar
+    Sidebar,
+    Alert
   },
   created () {
     const myCookie = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=s*([^;]*).*$)|^.*$/, '$1')
--- vue-pj/src/views/backEnd/Products.vue
+++ vue-pj/src/views/backEnd/Products.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <loading :active.sync="isLoading" :loader="dots"></loading>
+    <loading :active.sync="isLoading"></loading>
     <div class="text-right mt-4">
       <button type="button" class="btn btn-primary" @click="openModal(true)">
         建立新產品
@@ -343,7 +343,7 @@ export default {
           console.log(response.data)
           vm.status.fileUploading = false
           if (response.data.success) {
-            // vm.tempProduct.imageUrl = response.data;
+            vm.tempProduct.imageUrl = response.data
             vm.$set(vm.tempProduct, 'imageUrl', response.data.imageUrl)
           } else {
             this.$bus.$emit('message:push', response.data.message, 'danger')
